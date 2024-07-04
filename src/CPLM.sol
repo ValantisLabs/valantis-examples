715069f01f43d550eeaa433d78f84b9b7f498062
Tanishk Goyal <goyaltanishk02@gmail.com>
2024-07-04 15:22:19 +0200

    feat: first prototype of CPLM

--- src/CPLM.sol
+++ src/CPLM.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.8.19;
+pragma solidity ^0.8.20;
 
 import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
@@ -10,18 +10,24 @@ import { SafeERC20 } from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.s
 import { ISovereignALM } from '@valantis-core/ALM/interfaces/ISovereignALM.sol';
 import { ALMLiquidityQuoteInput, ALMLiquidityQuote } from '@valantis-core/ALM/structs/SovereignALMStructs.sol';
 import { ISovereignPool } from '@valantis-core/pools/interfaces/ISovereignPool.sol';
+import { SovereignPool } from '@valantis-core/pools/SovereignPool.sol';
 
 contract CPLM is ISovereignALM, ERC20 {
-    uint256 public constant MINIMUM_LIQUIDITY = 1e3;
-    uint256 public liquidity;
+    using SafeERC20 for IERC20;
 
-    constructor(string calldata name, string calldata _symbol, SovereignPool _pool) ERC20(_name, _symbol) {
+    uint256 public constant MINIMUM_LIQUIDITY = 1e3;
+
+    SovereignPool public pool;
+
+    error CPLM__onlyPool();
+
+    constructor(string memory _name, string memory _symbol, SovereignPool _pool) ERC20(_name, _symbol) {
         pool = _pool;
     }
 
     modifier onlyPool() {
-        if (msg.sender != pool) {
-            revert MockSovereignALM__onlyPool();
+        if (msg.sender != address(pool)) {
+            revert CPLM__onlyPool();
         }
         _;
     }
@@ -30,43 +36,33 @@ contract CPLM is ISovereignALM, ERC20 {
         uint256 _shares,
         address _recipient,
         bytes memory _verificationContext
-    ) external returns (uint256 amount0Deposited, uint256 amount1Deposited) {
+    ) external returns (uint256 amount0, uint256 amount1) {
         (uint256 reserve0, uint256 reserve1) = pool.getReserves();
 
         uint256 _totalSupply = totalSupply();
 
-        uint256 amount0;
-        uint256 amount1;
-
+        // First deposit must be donated directly to the pool
         if (_totalSupply == 0) {
+            amount0 = IERC20(pool.token0()).balanceOf(address(this));
+            amount1 = IERC20(pool.token1()).balanceOf(address(this));
+
             _mint(address(0), MINIMUM_LIQUIDITY);
-            shares = shares - MINIMUM_LIQUIDITY;
 
-            amount0 = token0.balanceOf(address(this));
-            amount1 = token1.balanceOf(address(this));
-
-            // First deposit must be donated directly to the pool
-            if (amount0 == 0 || amount1 > 0) {
-                require(false); // TODO: Change to revert
-            }
-
-            liquidity = Math.sqrt(amount0, amount1) - MINIMUM_LIQUIDITY;
+            _shares = (amount0 * amount1) - MINIMUM_LIQUIDITY;
         } else {
             amount0 = Math.mulDiv(reserve0, _shares, _totalSupply);
             amount1 = Math.mulDiv(reserve1, _shares, _totalSupply);
 
             ISovereignPool(pool).depositLiquidity(
-                _amount0,
-                _amount1,
+                amount0,
+                amount1,
                 msg.sender,
                 _verificationContext,
                 abi.encode(msg.sender)
             );
         }
 
-        _mint(msg.sender, shares);
-
-        liquidity = _totalSupply();
+        _mint(_recipient, _shares);
     }
 
     function burn(uint256 _shares, address _recipient, bytes memory _verificationContext) external {
@@ -80,12 +76,12 @@ contract CPLM is ISovereignALM, ERC20 {
 
         (uint256 reserve0, uint256 reserve1) = pool.getReserves();
 
-        amount0 = Math.mulDiv(reserve0, _shares, totalSupply());
-        amount1 = Math.mulDiv(reserve1, _shares, totalSupply());
+        uint256 amount0 = Math.mulDiv(reserve0, _shares, totalSupply());
+        uint256 amount1 = Math.mulDiv(reserve1, _shares, totalSupply());
 
-        _burn(msg.sender, shares);
+        _burn(msg.sender, _shares);
 
-        ISovereignPool(pool).withdrawLiquidity(_amount0, _amount1, msg.sender, _recipient, _verificationContext);
+        ISovereignPool(pool).withdrawLiquidity(amount0, amount1, msg.sender, _recipient, _verificationContext);
     }
 
     function onDepositLiquidityCallback(
@@ -95,32 +91,38 @@ contract CPLM is ISovereignALM, ERC20 {
     ) external override onlyPool {
         address user = abi.decode(_data, (address));
 
-        (address token0, address token1) = (ISovereignPool(pool).token0(), ISovereignPool(pool).token1());
-
         if (_amount0 > 0) {
-            IERC20(token0).safeTransferFrom(user, msg.sender, _amount0);
+            IERC20(pool.token0()).safeTransferFrom(user, msg.sender, _amount0);
         }
 
         if (_amount1 > 0) {
-            IERC20(token1).safeTransferFrom(user, msg.sender, _amount1);
+            IERC20(pool.token1()).safeTransferFrom(user, msg.sender, _amount1);
         }
     }
+    
+    // TODO: add onlyPool if any state modifying function is added
+    function onSwapCallback(bool _isZeroToOne, uint256 _amountIn, uint256 _amountOut) external override  {
 
+    }
+
+    // TODO: add onlyPool if any state modifying function is added
     function getLiquidityQuote(
         ALMLiquidityQuoteInput memory _poolInput,
         bytes calldata,
         bytes calldata
-    ) external override onlyPool returns (ALMLiquidityQuote memory quote) {
+    ) external view override returns (ALMLiquidityQuote memory quote) {
         (uint256 reserve0, uint256 reserve1) = pool.getReserves();
 
         quote.isCallbackOnSwap = false;
 
+        uint256 k = reserve0 * reserve1;
+
         if (_poolInput.isZeroToOne) {
-            quote.amountOut = reserve1 - liquidity / (reserve0 + _poolInput.amountInMinusFee);
+            quote.amountOut = reserve1 - k / (reserve0 + _poolInput.amountInMinusFee);
         } else {
-            quote.amountOut = reserve0 - liquidity / (reserve1 + _poolInput.amountInMinusFee);
+            quote.amountOut = reserve0 - k / (reserve1 + _poolInput.amountInMinusFee);
         }
 
-        quote.amountInFilled = _almLiquidityQuotePoolInput.amountInMinusFee;
+        quote.amountInFilled = _poolInput.amountInMinusFee;
     }
 }
